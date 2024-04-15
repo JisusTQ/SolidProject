ae4294f1f329b1929bf539c757cfe13320b55aec
JisusTQ <jesus.torresq@udea.edu.co>
2024-04-14 19:43:58 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/solidproject/SolidProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solidproject;
+
+/**
+ *
+ * @author jisus
+ */
+public class SolidProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

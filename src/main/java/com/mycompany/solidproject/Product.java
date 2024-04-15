e0d548d6aeb38569578d08d285e392c2c6de562c
JisusTQ <jesus.torresq@udea.edu.co>
2024-04-14 20:06:59 -0500

    create product class
    Create a getters and setters off product class
    
    Co-Authored-By: anagranada1 <85529165+anagranada1@users.noreply.github.com>

---
+++ src/main/java/com/mycompany/solidproject/Product.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.solidproject;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private float price;
+    private int stock;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+   
+    
+    
+}

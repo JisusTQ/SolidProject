f54f9bae488c08626ab4f0efc4036d50f89f3eb4
JisusTQ <jesus.torresq@udea.edu.co>
2024-05-01 18:10:05 -0500

    final commit
    Co-Authored-By: anagranada1 <85529165+anagranada1@users.noreply.github.com>

--- src/main/java/com/mycompany/solidproject/Connection.java
+++ src/main/java/com/mycompany/solidproject/Connection.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.solidproject;
 
 import com.mongodb.BasicDBObject;
@@ -18,10 +14,6 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import org.bson.Document;
 
-/**
- *
- * @author jisus
- */
 public class Connection {
 
     private MongoDatabase database;
--- src/main/java/com/mycompany/solidproject/ModelProductsTable.java
+++ src/main/java/com/mycompany/solidproject/ModelProductsTable.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.solidproject;
 
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
-/**
- *
- * @author Usuario
- */
 public class ModelProductsTable extends AbstractTableModel {
 
     List<Product> products;
